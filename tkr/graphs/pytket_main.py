39c620493f1ec98c209e2f38e2d5113f57109e22
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-06-18 15:26:06 +0100

    Fix imports

--- tkr/graphs/pytket_main.py
+++ tkr/graphs/pytket_main.py
@@ -3,7 +3,7 @@ from typing import Any, Literal, NamedTuple
 from uuid import UUID
 
 from aer_worker import submit_single
-from old_worker import append_pauli_measurements, optimise_ansatz, substitute
+from old_worker import append_pauli_measurement, optimise_ansatz, substitute
 from pytket import Qubit
 from pytket.circuit import Circuit, fresh_symbol
 from pytket.pauli import Pauli, QubitPauliString
@@ -54,9 +54,9 @@ def exp_val():
     pauli_string = g.inputs.pauli_string
     n_shots = g.inputs.n_shots
 
-    measurement_circuit = g.task(append_pauli_measurement_impl(circuit, pauli_string))
+    measurement_circuit = g.task(append_pauli_measurement(circuit, pauli_string))
 
-    compiled_circuit = g.task(optimise_phase_gadgets(measurement_circuit))
+    compiled_circuit = g.task(optimise_ansatz(measurement_circuit))
 
     backend_result = g.task(submit_single(compiled_circuit, n_shots))
     av = g.task(expectation(backend_result))
