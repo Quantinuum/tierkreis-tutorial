87e125c3e7b4c4a9b3f486c20e5a65b1aaaed73b
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-06-18 14:28:36 +0100

    update build

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,22 @@
 { pkgs, lib, ... }:
-
-{
-
-  packages = [
-    pkgs.nlohmann_json
+let
+  darwinRuntimeLibraries = with pkgs; [
+    libiconv
   ];
+  darwinRuntimeLibraryPath = lib.makeLibraryPath darwinRuntimeLibraries;
+in
+{
+ config = {
+  packages = [
+    pkgs.just
+    pkgs.zlib
+    pkgs.maturin
+    pkgs.bacon
+    pkgs.cargo-nextest
+    pkgs.sqlite
+    pkgs.nlohmann_json
+  ] ++ lib.optionals pkgs.stdenv.isDarwin darwinRuntimeLibraries;
+
 
   # https://devenv.sh/languages/
   languages.python = {
@@ -14,5 +26,15 @@
 
 
   languages.cplusplus.enable = true;
-
+   enterShell =
+    if pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64 then ''
+      unset UV_PYTHON;
+      export MATURIN_NO_PROGRESS=1
+      export RUST_LOG=error
+    '' else ''
+      export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+      export MATURIN_NO_PROGRESS=1
+      export RUST_LOG=error
+    '';
+ };
 }
