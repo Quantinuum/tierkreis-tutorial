094559540604f6d91ad1c09f79bbb502f69c7b60
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-06-22 09:27:32 +0200

    Fix api name

--- tkr/workers/cpp_worker/generate_stubs.py
+++ tkr/workers/cpp_worker/generate_stubs.py
@@ -4,5 +4,4 @@ from tierkreis.namespace import Namespace
 
 if __name__ == "__main__":
     namespace = Namespace.from_spec_file(Path(__file__).parent / "api.tsp")
-    namespace.name = "cpp_worker"
     namespace.write_stubs(Path(__file__).parent / "api.py")
