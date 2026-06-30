25c27adcab507ba8b315627557adb34f60c7a609
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-06-30 15:41:46 +0100

    Update graph

--- tkr/graphs/from_one_to_many.py
+++ tkr/graphs/from_one_to_many.py
@@ -3,23 +3,23 @@ from typing import Any, NamedTuple
 from uuid import UUID
 
 from cpp_worker import gen_inputs, Ansatz
-from guppy_worker import emulate, to_backend_result
 from guppylang import guppy
 from guppylang.std.angles import angle
 from guppylang.std.builtins import array
 from guppylang.std.quantum import cx, qubit, rz
-from reference_worker import append_pauli_measurements, optimise_ansatz, substitute
 from pytket import Qubit
 from pytket.pauli import Pauli, QubitPauliString
-from pytket_worker import expectation
 from tierkreis.builder import Graph
-from tierkreis.builtins import add, times, tkr_zip, untuple, unzip
 from tierkreis.controller import run_graph
-from tierkreis.controller.storage.debug_graph import debug_graph
 from tierkreis.executor import ShellExecutor
-from tierkreis.graphs.fold import FoldFunctionInput, FoldGraphInputs, fold_graph
 from tierkreis.models import OpaqueType, TKR, Workflow
 from tierkreis.storage import FileStorage, read_outputs
+
+
+from guppy_main import (
+    hamiltonian_sim as old_sim,
+    SymbolicExecutionInputs as OldSymbolicExecutionInputs,
+)
 
 
 @guppy(link_name="ansatz")
@@ -37,42 +37,6 @@ def ansatz(qbts: array[qubit, 4], a: float, b: float, c: float) -> None:  # type
         cx(qbts[i], qbts[i + 1])
 
 
-class SubmitInputs(NamedTuple):
-    circuit: TKR[OpaqueType["hugr.package.Package"]]
-    pauli_string: TKR[OpaqueType["pytket._tket.pauli.QubitPauliString"]]
-    n_shots: TKR[int]
-
-
-def exp_val():
-    g = Graph(SubmitInputs, TKR[float])
-
-    circuit = g.inputs.circuit
-    pauli_string = g.inputs.pauli_string
-    n_shots = g.inputs.n_shots
-
-    measurement_circuit = g.task(append_pauli_measurements(circuit, pauli_string))
-    compiled_circuit = g.task(optimise_ansatz(measurement_circuit))
-    result = g.task(emulate(compiled_circuit, g.const(4), n_shots))
-    backend_result = g.task(to_backend_result(result))
-    av = g.task(expectation(backend_result))
-    return g.finish_with_outputs(av)
-
-
-ComputeTermsInputs = FoldFunctionInput[
-    tuple[float, float], float
-]  # (value: (x,y) , accum: z) -> new_accum
-
-
-def compute_terms():
-    g = Graph(ComputeTermsInputs, TKR[float])
-
-    res_0, res_1 = g.task(untuple(g.inputs.value))
-    prod = g.task(times(res_0, res_1))
-    sum = g.task(add(g.inputs.accum, prod))
-
-    return g.finish_with_outputs(sum)
-
-
 class SymbolicExecutionInputs(NamedTuple):
     value: TKR[
         str
@@ -87,26 +51,19 @@ def hamiltonian_sim() -> Workflow[SymbolicExecutionInputs, TKR[float]]:
         gen_inputs(simulation_graph.inputs.value)
     )
 
-    substituted_ansatz = simulation_graph.task(
-        substitute(
-            simulation_graph.inputs.ansatz,
+    embedded = simulation_graph.embed(
+        old_sim(),
+        OldSymbolicExecutionInputs(
             ansatz_inputs.a,
             ansatz_inputs.b,
             ansatz_inputs.c,
-        )
+            simulation_graph.inputs.ham,
+            simulation_graph.inputs.ansatz,
+        ),
+        TKR[float],
     )
-    pauli_strings_list, parameters_list = simulation_graph.task(
-        unzip(simulation_graph.inputs.ham)
-    )
-    input_circuits = simulation_graph.map(
-        lambda x: SubmitInputs(substituted_ansatz, x, simulation_graph.const(100)),
-        pauli_strings_list,
-    )
-    exp_values = simulation_graph.map(exp_val(), input_circuits)
-    tuple_values = simulation_graph.task(tkr_zip(exp_values, parameters_list))
-    fold_inputs = FoldGraphInputs(simulation_graph.const(0.0), tuple_values)
-    computed = simulation_graph.eval(fold_graph(compute_terms()), fold_inputs)
-    return simulation_graph.finish_with_outputs(computed)
+
+    return simulation_graph.finish_with_outputs(embedded)
 
 
 class MultipleHamiltoniansInputs(NamedTuple):
